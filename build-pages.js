6acd4fd181b64a3e25fdebbe1d38de426a70dcd3
Kirill <b14b14@yandex.ru>
2023-02-21 17:30:35 +0300

    small fixes

--- build-pages.js
+++ build-pages.js
@@ -16,16 +16,16 @@ const ncp = promisify(NCP.ncp)
 const pages = [
     'index',
     'compare'
-    /*'play',
-    'docs',
-    'modules',
-    'editors',
-    'faq'*/
-    
+    //'docs'
+    //'play',
+    //'modules',
+    //'editors',
+    // 'faq' 
+
 ]
 
 async function compile() {
-   /* console.log('Building docs')
+    /*console.log('Building docs')
 
     console.log('Setting up MD parser')
     var md = require('markdown-it')({})
@@ -36,18 +36,18 @@ async function compile() {
             .replace(/"/g, '\\"')
         if (code.endsWith('\\n')) code = code.slice(0, -2)
         return `{% set code = "${code}" %}
-{% set noheader = true  %}
-{% include '../components/codeblock.njk' %}
-{% set noheader = false %}\n`
+    {% set noheader = true  %}
+    {% include '../components/codeblock.njk' %}
+    {% set noheader = false %}\n`
     }
 
     console.log('Fetching MD from github')
-    let docsMD = await fetch('https://raw.githubusercontent.com/vlang/v/master/doc/docs.md')
+    let docsMD = await fetch('https://raw.githubusercontent.com/vlang/v/master/doc/docs.md') 
 
     console.log('Building NJK from MD')
     docsMD = await docsMD.text()
     let docs = md.render(docsMD)
-    await writeFile('./pages/docs/content.njk', docs)
+    await writeFile('./pages/docs/content.njk', docs) 
 
     console.log('Generating TOC')
     let toc = mdtoc(docsMD, { slugify: require('uslug') }).content
@@ -62,37 +62,32 @@ async function compile() {
     let lin = release.assets.find(r => r.name === 'v_linux.zip')
     let mac = release.assets.find(r => r.name === 'v_macos.zip')
     let versions = `{% set lin_size = "${Math.round((lin.size / 1024 / 1024)*10)/10}MB" %}
-{% set mac_size = "${Math.round((mac.size / 1024 / 1024)*10)/10}MB" %}
-{% set win_size = "${Math.round((win.size / 1024 / 1024)*10)/10}MB" %}
-{% set version = "${release.name}" %}`
+    {% set mac_size = "${Math.round((mac.size / 1024 / 1024)*10)/10}MB" %}
+    {% set win_size = "${Math.round((win.size / 1024 / 1024)*10)/10}MB" %}
+    {% set version = "${release.name}" %}`
     await writeFile('./pages/home/download-versions.njk', versions)
-
     console.log('Building pages')
     let buildDir = "./build"
     await fs.rmSync(buildDir, { recursive: true, force: true });
-      console.log('Deleting old build folder if exists')
+    console.log('Deleting old build folder if exists')
     await fs.promises.mkdir(buildDir, { recursive: true })
-      console.log('Creating new build folder')
+    console.log('Creating new build folder')
     await Promise.all(
         pages.map(page =>
             writeFile(
                 path.join(buildDir, page + ".html"),
-                nunjucks.render(path.join("./pages", page + ".njk")),
+                nunjucks.render(path.join("./pages", page + ".njk")), // this doesnt work for docs.njk
                 err => err && console.error(err)
             )
-
         )
     )
-
     console.log('Building sass')
     let app_sass = await renderSass({ file: './app.sass' })
     let clean_css = new CleanCSS({}).minify(app_sass.css.toString())
     await writeFile("./build/app.css", clean_css.styles, 'utf8')
-
     console.log('Copying res')
     ncp('./res', './build')
-
- console.log('Serving page on localhost:3000 via serve cause of "& serve build/" from line 23 of package.json')
+    console.log('Serving page on localhost:3000 via serve cause of "& serve build/" from line 23 of package.json')
 }
 
 compile()
